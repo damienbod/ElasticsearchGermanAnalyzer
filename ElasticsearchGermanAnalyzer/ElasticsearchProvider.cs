4a1ebfaa6f473b3ecd84f05ea7aa092d791206e4
damien <damien.bowden@noser.com>
2014-12-20 20:59:59 +0100

    init

---
+++ ElasticsearchGermanAnalyzer/ElasticsearchProvider.cs
@@ -0,0 +1,115 @@
+﻿using ElasticsearchCRUD;
+using ElasticsearchCRUD.ContextAddDeleteUpdate.IndexModel.SettingsModel;
+using ElasticsearchCRUD.ContextSearch.SearchModel;
+using ElasticsearchCRUD.Tracing;
+
+namespace ElasticsearchGermanAnalyzer
+{
+	public class ElasticsearchGermanSearchProvider
+	{
+		public ElasticsearchGermanSearchProvider()
+		{
+			_context = new ElasticsearchContext(ConnectionString, new ElasticsearchSerializerConfiguration(_elasticsearchMappingResolver))
+			{
+				TraceProvider = new ConsoleTraceProvider()
+			};
+		}
+		private readonly IElasticsearchMappingResolver _elasticsearchMappingResolver = new ElasticsearchMappingResolver();
+		private const string ConnectionString = "http://localhost.fiddler:9200";
+		private readonly ElasticsearchContext _context;
+
+		public void CreateIndex(IndexDefinition indexDefinition)
+		{			
+			_context.IndexCreate<GermanData>(indexDefinition);
+		}
+
+		public void CreateSomeMembers()
+		{
+			var jm = new GermanData {Id = 1, FamilyName = "Moore", Info = "Muenich", Name = "John"};
+			_context.AddUpdateDocument(jm, jm.Id);
+			var jj = new GermanData { Id = 2, FamilyName = "Jones", Info = "Münich", Name = "Johny" };
+			_context.AddUpdateDocument(jj, jj.Id);
+			var pm = new GermanData { Id = 3, FamilyName = "Murphy", Info = "Munich", Name = "Paul" };
+			_context.AddUpdateDocument(pm, pm.Id);
+			var sm = new GermanData { Id = 4, FamilyName = "McGurk", Info = "munich", Name = "Séan" };
+			_context.AddUpdateDocument(sm, sm.Id);
+			var sob = new GermanData { Id = 5, FamilyName = "O'Brien", Info = "Not a much use, bit of a problem", Name = "Sean" };
+			_context.AddUpdateDocument(sob, sob.Id);
+			var tmc = new GermanData { Id = 6, FamilyName = "McCauley", Info = "Couldn't a ask for anyone better", Name = "Tadhg" };
+			_context.AddUpdateDocument(tmc, tmc.Id);
+
+			_context.SaveChanges();
+		}
+
+		//{
+		//  "query": {
+		//		"match": {"name": "sean"}
+		//	 }
+		//  }
+		//}
+		public SearchResult<GermanData> Search(string name)
+		{
+			var query = "{ \"query\": { \"match\": {\"info\": \""+ name + "\"} }  }";
+			return _context.Search<GermanData>(query).PayloadResult;
+		}
+
+	}
+}
+
+
+
+//			{
+		//  "filter": {
+		//	"de_stop_filter": {
+		//	  "type": "stop",
+		//	  "stopwords": ["_german_"]
+		//	},
+		//	"de_stem_filter": {
+		//	  "type": "stemmer",
+		//	  "name": "minimal_german"
+		//  },
+		//  "analyzer": {
+			//	"custom_german_icu": {
+		//	  "type": "custom",
+		//	  "filter": ["icu_normalizer", "de_stop_filter", "de_stem_filter", "icu_folding"],
+		//	  "tokenizer": "icu_tokenizer"
+		//  }
+		//}
+
+			//return new IndexDefinition
+			//{
+			//	IndexSettings =
+			//	{
+			//		Analysis = new Analysis
+			//		{
+			//			Filters= new AnalysisFilter
+			//			{
+			//				CustomFilters= new List<AnalysisFilterBase>
+			//				{
+			//					new StopTokenFilter("de_stop_filter")
+			//					{
+			//						StopwordsList = new List<string>{"_german_"}
+			//					}, 
+			//					new StemmerTokenFilter("de_stem_filter")
+			//					{
+			//						StemmerName = Stemmer.minimal_german
+			//					}
+			//				}
+			//			},
+			//			Analyzer =
+			//			{
+			//				Analyzers = new List<AnalyzerBase>
+			//				{
+			//					new CustomAnalyzer("custom_german_icu")
+			//					{
+			//						Tokenizer = "icu_tokenizer",
+			//						Filter= new List<string>{"icu_normalizer", "de_stop_filter", "de_stem_filter", DefaultTokenFilters.IcuFolding},
+			//					}
+								
+			//				}
+			//			}
+			//		},
+			//		NumberOfShards = 3,
+			//		NumberOfReplicas = 1
+			//	},
+			//};
---
+++ ElasticsearchGermanAnalyzer/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using ElasticsearchCRUD.ContextAddDeleteUpdate.CoreTypeAttributes;
+using ElasticsearchCRUD.ContextAddDeleteUpdate.IndexModel.SettingsModel;
+using ElasticsearchCRUD.Model;
+
+namespace ElasticsearchGermanAnalyzer
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var elasticsearchGermanSearchProvider = new ElasticsearchGermanSearchProvider();
+
+			// Create the index defintion
+			//var indexDefinition = elasticsearchGermanSearchProvider.CreateNewIndexDefinitionForGermanSearch();
+
+			// create a new index and type mapping in elasticseach
+			elasticsearchGermanSearchProvider.CreateIndex( new IndexDefinition());
+			Console.ReadLine();
+
+			elasticsearchGermanSearchProvider.CreateSomeMembers();
+			Console.ReadLine();
+
+			//{
+			//  "query": {
+			//		"match": {"info": "munich"}
+			//	 }
+			//  }
+			//}
+
+			//{
+			//  "query": {
+			//	"match": {
+			//	  "info": {
+			//		"query": "munich",
+			//		"fuzziness": 2,
+			//		"prefix_length": 1
+			//	  }
+			//	}
+			//  }
+			//}
+
+			//{
+			//		"filter" : {
+			//			"query" : {
+			//				"query_string" : {
+			//					"query" : "münich",
+			//					"analyzer" : "german"
+			//				}
+			//			}
+			//		}
+			//}
+
+			// we expect 4 results for each of the following searches
+			var resultSean = elasticsearchGermanSearchProvider.Search("munich");
+			Console.WriteLine("Found for sean Total: {0}", resultSean.Hits.Total);
+			Console.ReadLine();
+
+			var resultJohny = elasticsearchGermanSearchProvider.Search("Muenich");
+			Console.WriteLine("Found for Johny Total: {0}", resultJohny.Hits.Total);
+			Console.ReadLine();
+
+			var resultSeanWithFada = elasticsearchGermanSearchProvider.Search("Münich");
+			Console.WriteLine("Found for Séan Total: {0}", resultSeanWithFada.Hits.Total);
+
+			// http://localhost:9200/germandatas/_analyze?&analyzer=german&text=Muenich%20munich%20m%C3%BCnich%20M%C3%BCnich
+
+			Console.ReadLine();
+		}
+	}
+
+	public class GermanData
+	{
+		public long Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string FamilyName { get; set; }
+
+		[ElasticsearchString(Fields = typeof(FieldDataDefinition), Analyzer=LanguageAnalyzers.German)]
+		public string Info { get; set; }
+	}
+
+	
+	public class FieldDataDefinition
+	{
+		[ElasticsearchString(Index=StringIndex.not_analyzed)]
+		public string Raw { get; set; }	
+	}
+}
